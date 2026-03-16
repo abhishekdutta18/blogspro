234f21f9325a74648ebb92dff3adfbfdcd1280b4
abhishekdutta18 <87544669+abhishekdutta18@users.noreply.github.com>
2026-03-16 21:37:07 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,34 +1,63 @@
-// Firebase Remote Config loader
-
-import { app } from "./firebase.js";
+import { remoteConfig } from "./firebase.js";
 
 import {
-  getRemoteConfig,
   fetchAndActivate,
   getValue
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-remote-config.js";
 
+
 export let AI_KEYS = {};
+
 
 export async function loadRemoteConfig() {
 
-  const rc = getRemoteConfig(app);
+  try {
 
-  rc.settings = {
-    minimumFetchIntervalMillis: 3600000
-  };
+    remoteConfig.settings = {
+      minimumFetchIntervalMillis: 3600000
+    };
 
-  await fetchAndActivate(rc);
+    await fetchAndActivate(remoteConfig);
 
-  AI_KEYS = {
-    cloudflare: getValue(rc, "cloudflare_key").asString(),
-    groq: getValue(rc, "groq_key").asString(),
-    openrouter: getValue(rc, "openrouter_key").asString(),
-    together: getValue(rc, "together_key").asString(),
-    deepinfra: getValue(rc, "deepinfra_key").asString(),
-    gemini: getValue(rc, "gemini_key").asString()
-  };
+    AI_KEYS = {
 
-  console.log("[config] Remote Config loaded. Keys:", Object.keys(AI_KEYS));
+      cloudflare: getValue(remoteConfig, "cloudflare_key").asString(),
+      groq: getValue(remoteConfig, "groq_key").asString(),
+      openrouter: getValue(remoteConfig, "openrouter_key").asString(),
+      together: getValue(remoteConfig, "together_key").asString(),
+      deepinfra: getValue(remoteConfig, "deepinfra_key").asString(),
+      gemini: getValue(remoteConfig, "gemini_key").asString()
+
+    };
+
+    console.log("[config] Remote Config loaded");
+
+  }
+
+  catch (err) {
+
+    console.warn("[config] Remote Config failed", err);
+
+  }
+
+}
+
+
+
+/* ------------------------------------------------
+Editor HTML Cleaner
+------------------------------------------------ */
+
+export function cleanEditorHTML(html) {
+
+  if (!html) return "";
+
+  html = html.replace(/<script.*?>.*?<\/script>/gi, "");
+
+  html = html.replace(/<p>\s*<\/p>/g, "");
+
+  html = html.replace(/\n\s*\n/g, "\n");
+
+  return html.trim();
 
 }
