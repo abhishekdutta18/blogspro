0083d7a19a0445fb89f9fb6279e1cf6d43cdadf3
abhishekdutta18 <87544669+abhishekdutta18@users.noreply.github.com>
2026-03-16 21:21:44 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,12 @@
-import { getRemoteConfig, fetchAndActivate, getValue } from "firebase/remote-config";
+// Firebase Remote Config loader
+
 import { app } from "./firebase.js";
+
+import {
+  getRemoteConfig,
+  fetchAndActivate,
+  getValue
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-remote-config.js";
 
 export let AI_KEYS = {};
 
@@ -14,14 +21,12 @@ export async function loadRemoteConfig() {
   await fetchAndActivate(rc);
 
   AI_KEYS = {
-
     cloudflare: getValue(rc, "cloudflare_key").asString(),
     groq: getValue(rc, "groq_key").asString(),
     openrouter: getValue(rc, "openrouter_key").asString(),
     together: getValue(rc, "together_key").asString(),
     deepinfra: getValue(rc, "deepinfra_key").asString(),
     gemini: getValue(rc, "gemini_key").asString()
-
   };
 
   console.log("[config] Remote Config loaded. Keys:", Object.keys(AI_KEYS));
